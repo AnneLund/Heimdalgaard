4f460d83b2a55456fd33f0aea71d8a6d7ecdc6e2
AnneLund <89774650+AnneLund@users.noreply.github.com>
2022-03-09 08:51:59 +0100

    ...

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,70 +8,65 @@ burgerMenu.addEventListener("click", function () {
   overlay.classList.toggle("overlay");
 });
 
- //FORMULAR
+
  
- //Jeg deklarerer en konstant og tildeler den en key "button".
- const button = document.getElementById("send");
+//  //Hvis inputtet "Fulde navn" er tomt, skal brugeren informeres om dette med en alert. Feltet skal markeres, så brugeren ved, hvor informationen mangler (name.focus)
  
- //Når jeg klikker på knappen "send", vil jeg have en funktion som har flere conditions.
- button.onclick = function () {
- 
- //Hvis inputtet "Fulde navn" er tomt, skal brugeren informeres om dette med en alert. Feltet skal markeres, så brugeren ved, hvor informationen mangler (name.focus)
- 
-   const name = document.getElementById("name");
-   if (name.value === "") {
-     alert("Udfyld feltet fulde navn!");
-     name.focus();
-     return false;
-   }
+//    const name = document.getElementById("name");
+//    if (name.value === "") {
+//      alert("Udfyld feltet fulde navn!");
+//      name.focus();
+//    }
 
  
  
- //Email-validering: Hvis inputtet "email" ikke er udfyldt, skal brugeren have en alert,
-   const email = document.getElementById("email");
-   if (!email.value) {
-     alert("Udfyld emailadresse!");
-     email.focus();
-     return false;
-   } else {
+//  //Email-validering: Hvis inputtet "email" ikke er udfyldt, skal brugeren have en alert,
+//    const email = document.getElementById("email");
+//    if (!email.value) {
+//      alert("Udfyld emailadresse!");
+//      email.focus();
+    
+//    } else {
  
- //Hvis brugerens indtastning af email ikke lever op til det pattern der er beskrevet i den tilhørende funktion, skal brugeren have en alert.
-     if (!isValidEmail(email.value)) {
-       alert("Indtast korrekt emailadresse");
-       email.focus();
-       return false;
-     }
-   }
+//  //Hvis brugerens indtastning af email ikke lever op til det pattern der er beskrevet i den tilhørende funktion, skal brugeren have en alert.
+//      if (!isValidEmail(email.value)) {
+//        alert("Indtast korrekt emailadresse");
+//        email.focus();
+     
+//      }
+//    }
 
-     checkNumber();
+//      checkNumber();
  
  
- //Hvis brugeren ikke har angivet sit nummer, skal denne have en alert.
-   function checkNumber() {
-     const number = document.getElementById("number");
-   }
+//  //Hvis brugeren ikke har angivet sit nummer, skal denne have en alert.
+//    function checkNumber() {
+//      const number = document.getElementById("number");
+//    }
 
-   const message = document.getElementById("message");
-   if(message.value === "") {
-     alert("Skriv din besked i tekstfeltet");
-     message.focus();
-     return false;
-   }
+//    const message = document.getElementById("message");
+//    if(message.value === "") {
+//      alert("Skriv din besked i tekstfeltet");
+//      message.focus();
+    
+//    }
  
- //Funktion der checker, at brugeren har indtastet et 8-cifret nummer.
-   function isValidNumber(value) {
-     let pattern = /^\d{8}$/;
-     return pattern.test(value);
-   }
+//  //Funktion der checker, at brugeren har indtastet et 8-cifret nummer.
+//    function isValidNumber(value) {
+//      let pattern = /^\d{8}$/;
+//      return pattern.test(value);
+//    }
  
- //Funktion der checker at emailen indeholder de nødvendige tegn.
-   function isValidEmail(value) {
-     let pattern =
-       /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-     return pattern.test(value);
-   }
+//  //Funktion der checker at emailen indeholder de nødvendige tegn.
+//    function isValidEmail(value) {
+//      let pattern =
+//        /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+//      return pattern.test(value);
+//    }
  
- //Brugeren får også en alert, der fortæller at formularen er afsendt, hvis der ikke er krydset af i checkboxen.
-   alert("Formularen blev sendt!");
- };
+//  //Brugeren får også en alert, der fortæller at formularen er afsendt, hvis der ikke er krydset af i checkboxen.
+//    alert("Formularen blev sendt!");
+//  ;
+
+ 
  
